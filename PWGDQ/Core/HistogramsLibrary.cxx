74b226a3df180739983d2a104361a2d36a24388a
Yuanjing Ji <yji@lxbk1130.gsi.de>
2025-02-26 14:34:29 +0100

    update hists

--- PWGDQ/Core/HistogramsLibrary.cxx
+++ PWGDQ/Core/HistogramsLibrary.cxx
@@ -841,6 +841,7 @@ void o2::aod::dqhistograms::DefineHistograms(HistogramManager* hm, const char* h
     if (subGroupStr.Contains("tpcpidvstofpid")) {
       hm->AddHistogram(histClass, "tpcNSigmaKa_tofNSigmaKa", "", false, 200, -10., 10., VarManager::kTPCnSigmaKa, 200, -10., 10., VarManager::kTOFnSigmaKa);
       hm->AddHistogram(histClass, "tpcNSigmaPi_tofNSigmaPi", "", false, 200, -10., 10., VarManager::kTPCnSigmaPi, 200, -10., 10., VarManager::kTOFnSigmaPi);
+      hm->AddHistogram(histClass, "tpcNSigmaPr_tofNSigmaPr_P", "", false, 200, -10., 10., VarManager::kTPCnSigmaPr, 200, -10., 10., VarManager::kTOFnSigmaPi, 50, 0, 5, VarManager::kP);
     }
   }
 
